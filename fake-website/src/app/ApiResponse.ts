db66a7d8bb00e2de32c31ba01eed80d6fac07f23
Carlsson David <David.Carlsson@csn.se>
2022-07-14 17:14:04 +0200

    get function and cards working

---
+++ fake-website/src/app/ApiResponse.ts
@@ -0,0 +1,13 @@
+export interface ApiResponse {
+    messages: {
+        shortId: number;
+        headline: string;
+        text: string;
+        hyperlink: string;
+        author: string;
+        startDate: string;
+        endDate: string;
+        publishingDate: string;
+        logo: string;
+    }[];
+}
