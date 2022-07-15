ba562e8968dfd96756f85a0b1b20d91936084f7f
csnmiki <mikolaj.wagner@csn.se>
2022-07-15 13:52:15 +0200

    La till lite grejor allt borde vara gött

--- fake-website/src/app/app.module.ts
+++ fake-website/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessageComponent } from './message/message.component';
+import { ShowMesComponent } from './message/show-mes/show-mes.component';
+import { SharedService } from './shared.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageComponent,
+    ShowMesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
