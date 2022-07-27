46daff630f7f9e10f725e3d8855535cfd0b9fa0d
Carlsson David <David.Carlsson@csn.se>
2022-07-27 15:00:19 +0200

    lookin good

--- fake-website/src/app/app.component.ts
+++ fake-website/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { CardComponent } from './card/card.component';
 import { ApiResponse } from './ApiResponse';
 
 @Component({
@@ -11,10 +10,8 @@ import { ApiResponse } from './ApiResponse';
 
 export class AppComponent implements OnInit { 
   title = 'fake-website';
-  selectedCard: CardComponent = new CardComponent;
   url: string = "http://localhost:4200/public/messages";
   messages: any;
-  /*htmlToAdd: any = '<app-card [card]="selectedCard" class="card"></app-card>';*/
 
   constructor(private _http: HttpClient) {}
 
